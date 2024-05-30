0794675194e4721197c80ca349f92f3e5f7411fb
madalasuma1@gmail.com <122592209+sumamada@users.noreply.github.com>
2024-05-30 11:16:36 +0530

    first commit

--- terraforms/1-ec2.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_instance" "demo-server" {
-    ami = "ami-0bb84b8ffd87024d8"
-    instance_type = "t2.micro"
-    key_name = "newpair"
-}
